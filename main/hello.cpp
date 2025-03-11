3310bd160a0e35e145f23f7ba1402b82fe67cf0d
diyahande <diyaryahande@gmail.com>
2025-03-11 15:26:00 +0530

    good day for a lab?

---
+++ main/hello.cpp
@@ -0,0 +1,10 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "I have so much to do in this lab!\n";
+  cout << "Doing it for the screenshots :(\n";
+  return 0;
+}
